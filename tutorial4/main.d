65522917b5b24426cc504d0225ad0ce53b48267b
TimurGafarov <gt@smarthead.ru>
2019-08-26 16:53:08 +0300

    Ported tutorial4

--- tutorial4/main.d
+++ tutorial4/main.d
@@ -4,17 +4,20 @@ import dagon;
 
 class TestScene: Scene
 {
+    Game game;
     OBJAsset aOBJSuzanne;
     TextureAsset aTexStoneDiffuse;
     TextureAsset aTexStoneNormal;
     TextureAsset aTexStoneHeight;
+    ShapeSphere lightSphere;
 
-    this(SceneManager smngr)
+    this(Game game)
     {
-        super(smngr);
+        super(game);
+        this.game = game;
     }
 
-    override void onAssetsRequest()
+    override void beforeLoad()
     {    
         aOBJSuzanne = addOBJAsset("data/suzanne.obj");
         aTexStoneDiffuse = addTextureAsset("data/stone-diffuse.png");
@@ -22,51 +25,81 @@ class TestScene: Scene
         aTexStoneHeight = addTextureAsset("data/stone-height.png");
     }
 
-    override void onAllocate()
+    override void afterLoad()
     {
-        super.onAllocate();
+        environment.ambientColor = Color4f(0.2, 0.2, 0.2, 1);
+        game.deferredRenderer.ssaoEnabled = true;
+        game.deferredRenderer.ssaoPower = 6.0;
+        game.postProcessingRenderer.glowEnabled = true;
+        game.postProcessingRenderer.fxaaEnabled = true;
         
-        view = New!Freeview(eventManager, assetManager);
+        auto camera = addCamera();
+        auto freeview = New!FreeviewComponent(eventManager, camera);
+        freeview.pitch(-30.0f);
+        freeview.turn(10.0f);
+        game.renderer.activeCamera = camera;
         
-        environment.setDayTime(00, 00, 00);
+        auto matSuzanne = New!Material(assetManager);
+        matSuzanne.diffuse = Color4f(1.0, 1.0, 1.0, 1.0);
         
-        auto matSuzanne = createMaterial();
-        matSuzanne.diffuse = Color4f(1.0, 0.2, 0.2, 1.0);
-        
-        auto matGround = createMaterial();
+        auto matGround = New!Material(assetManager);
         matGround.diffuse = aTexStoneDiffuse.texture;
         matGround.normal = aTexStoneNormal.texture;
         matGround.height = aTexStoneHeight.texture;
+        matGround.roughness = 0.2f;
+        matGround.textureScale = Vector2f(2, 2);
 
-        auto eSuzanne = createEntity3D();
+        auto eSuzanne = addEntity();
         eSuzanne.drawable = aOBJSuzanne.mesh;
         eSuzanne.material = matSuzanne;
         eSuzanne.position = Vector3f(0, 1, 0);
         
-        auto ePlane = createEntity3D();
+        auto ePlane = addEntity();
         ePlane.drawable = New!ShapePlane(10, 10, 1, assetManager);
         ePlane.material = matGround;
         
-        auto light1 = createLightSphere(Vector3f(-2, 1, 0), Color4f(1.0f, 1.0f, 0.0f), 10.0f, 5.0f, 0.1f);
-        auto light2 = createLightSphere(Vector3f(2, 1, 0), Color4f(0.0f, 1.0f, 1.0f), 10.0f, 5.0f, 0.1f);
+        lightSphere = New!ShapeSphere(1.0f, 24, 16, false, assetManager);
+        
+        addLightBall(Vector3f(-3, 1, 0), Color4f(1.0f, 0.5f, 0.0f), 10.0f, 0.1f, 5.0f);
+        addLightBall(Vector3f(3, 1, 0),  Color4f(0.0f, 0.5f, 1.0f), 10.0f, 0.2f, 5.0f);
+        addLightBall(Vector3f(0, 1, 3),  Color4f(0.0f, 1.0f, 0.0f), 10.0f, 0.3f, 5.0f);
+        addLightBall(Vector3f(0, 1, -3), Color4f(1.0f, 0.0f, 0.0f), 10.0f, 0.4f, 5.0f);
+    }
+    
+    Light addLightBall(Vector3f pos, Color4f color, float energy, float areaRadius, float volumeRadius)
+    {
+        auto light = addLight(LightType.AreaSphere);
+        light.castShadow = false;
+        light.position = pos;
+        light.color = color;
+        light.energy = energy;
+        light.radius = areaRadius;
+        light.volumeRadius = volumeRadius;
+
+        auto lightGeom = addEntity(light);
+        lightGeom.drawable = lightSphere;
+        lightGeom.scaling = Vector3f(areaRadius, areaRadius, areaRadius);
+        lightGeom.material = New!Material(assetManager);
+        lightGeom.material.diffuse = color;
+        lightGeom.material.emission = color;
+        lightGeom.material.energy = energy;
+
+        return light;
     }
 }
 
-class MyApplication: SceneApplication
+class MyGame: Game
 {
-    this(string[] args)
+    this(uint w, uint h, bool fullscreen, string title, string[] args)
     {
-        super("Dagon tutorial 4. Light sources", args);
-
-        TestScene test = New!TestScene(sceneManager);
-        sceneManager.addScene(test, "TestScene");
-        sceneManager.goToScene("TestScene");
+        super(w, h, fullscreen, title, args);
+        currentScene = New!TestScene(this);
     }
 }
 
 void main(string[] args)
 {
-    MyApplication app = New!MyApplication(args);
-    app.run();
-    Delete(app);
+    MyGame game = New!MyGame(1280, 720, false, "Dagon tutorial 4. Light sources", args);
+    game.run();
+    Delete(game);
 }
